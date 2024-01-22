1b69cb391e9d624138a0d7004ce86e7340ca3ba9
Guacimara Ramirez Santana <guaci.rs10@gmail.com>
2024-01-22 18:28:40 +0000

    DATABASE UPDATE

--- database/index.js
+++ database/index.js
@@ -3,12 +3,14 @@ require('dotenv').config()
 
 
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
-    host: 'localhost',
-    dialect: 'mysql',
-    port: process.env.DB_PORT,
-    logging: false,
-})
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: process.env.DB_PORT,
+//     logging: false,
+// })
+
+    const sequelize = new Sequelize (`${process.env.DIALECT}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 
 async function checkConnection(){
     try {
