84a13b365067f2948aa3c73e4ee44c2f541c1181
Guacimara Ramirez Santana <guaci.rs10@gmail.com>
2024-01-22 16:34:26 +0000

    deploy ready

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
+const sequelize = new Sequelize(`mysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}/${{MYSQL_DATABASE}}`,{
     host: 'localhost',
     dialect: 'mysql',
     port: process.env.DB_PORT,
